6c50d383dee3f3b60bca54d39435df0773cde2ee
koyo72-4 <32035846+koyo72-4@users.noreply.github.com>
2018-10-14 12:30:00 -0400

    installed Konva

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import Konva from 'konva';
+import { Stage, Layer, Line } from 'react-konva';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="map">
-          <Line />
+        <div id="map">
+          <MyMap />
         </div>
       </div>
     );
@@ -16,10 +18,30 @@ class App extends Component {
 export default App;
 
 
-class Line extends Component {
+class MyMap extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      endPoint: [100, 200]
+    }
+  }
+  changeEndPoint() {
+
+  }
   render() {
     return (
-      <div></div>
+      <Stage width={500} height={500}>
+        <Layer>
+          <Line
+            onClick={this.changeEndPoint}
+            x={5}
+            y={5}
+            points={[0, 0].concat(this.state.endPoint)}
+            closed
+            stroke="black"
+          />
+        </Layer>
+      </Stage>
     );
   }
 }
